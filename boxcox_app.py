d5511ab14b8d947f6181f48c13f82b8933fce874
Aglihareth <alexis.mk09@gmail.com>
2026-05-06 16:17:24 -0600

    Update boxcox_app.py

--- boxcox_app.py
+++ boxcox_app.py
@@ -36,7 +36,7 @@ st.markdown("""
     /* Header principal */
     .main-title {
         font-family: 'IBM Plex Mono', monospace;
-        font-size: 4.2rem;
+        font-size: 2.1rem !important;
         font-weight: 1200;
         color: #0f4c81;
         letter-spacing: -1px;
